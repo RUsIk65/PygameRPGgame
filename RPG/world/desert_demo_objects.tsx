bd07226a1353d53de8ad24056d91cc378543a61b
RUsIk65 <ruslanysupov65@gmail.com>
2026-05-21 03:18:30 +0500

    Delete RPG/world directory

--- RPG/world/desert_demo_objects.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="desert_demo_objects" tilewidth="16" tileheight="16" tilecount="55" columns="11">
- <image source="../The Fan-tasy Tileset (Free)/desert_demo_objects.png" width="176" height="80"/>
-</tileset>
