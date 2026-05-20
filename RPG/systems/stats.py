ed4203e59ed2004ea84fdacb2dbef2e5f5a648fe
RUsIk65 <ruslanysupov65@gmail.com>
2026-05-20 23:22:48 +0500

    Update stats.py

--- RPG/systems/stats.py
+++ RPG/systems/stats.py
@@ -5,103 +5,60 @@ class Stats:
 
     def __init__(
         self,
-        hp=100,
-        mana=50,
-        attack=10,
-        defense=5,
-        speed=4,
-        attack_range=50,
-        crit_chance=5
-        level=1
+        hp=0,
+        mana=0,
+        attack=0,
+        defense=0,
+        speed=0,
+        attack_range=0,
+        crit_chance=0,
+        level = 1
     ):
 
         # Уровень 
-        self.level = level
+        self._level = level
         self.xp = 0
-        self.xp_to_next_level = 100
+        self._xp_to_next_level = 100
 
-        
-        self._max_hp = int(self.max_hp * 1.20 * self.level)
-        self._max_mana = int(self.max_mana * 1.15 * self.level)
+        # БАЗОВЫЕ значения
+        self._base_hp = hp
+        self._base_mana = mana
+        self._base_attack = attack
+        self._base_defense = defense
 
-        self._attack = int(self.attack * 1.15 * self.level)
-        self._defense = int(self.defense * 1.10 * self.level)
+        # Расчёт с учётом уровня
+        self._max_hp = int(self._base_hp * 1.20 * self._level)
+        self._max_mana = int(self._base_mana * 1.15 * self._level)
+
+        self.current_hp = self._max_hp
+        self.current_mana = self._max_mana
+
+        self._attack = int(self._base_attack * 1.15 * self._level)
+        self._defense = int(self._base_defense * 1.10 * self._level)
 
         self._speed = speed
+        self._attack_range = attack_range
+        self._crit_chance = crit_chance
 
-        self._attack_range = attack_range 
-
-        self._xp_to_next_level = int(self.xp_to_next_level * 1.35 * self.level)
-
-        
-
-    
-    # система XP
-   
-
+        self._xp_to_next_level = int(self._xp_to_next_level * 1.35 * self._level)
+            
+    # система XP   
     def add_xp(self, amount):
         self.xp += amount
 
         while self.xp >= self.xp_to_next_level:
             self.xp -= self.xp_to_next_level
             self.level_up()
-
-    
+   
     # LEVEL UP
   
     def level_up(self):
         self.level += 1
-
-        #self.max_hp = int(self.max_hp * 1.20 * self.level)
         self.current_hp = self.max_hp
-
-        #self.max_mana = int(self.max_mana * 1.15)
         self.current_mana = self.max_mana
-
-
-        #self.attack = int(self.attack * 1.15)
-
-        #self.defense = int(self.defense * 1.10)
-
-        #self.speed = int(self.speed * 1.15)
-
-        # self.attack_range = int(self.attack_range * 1.05)
-
-        # self.xp_to_next_level = int(self.xp_to_next_level * 1.35)
 
         print(f"LEVEL UP! New level: {self.level}")
 
-    #система урона
-
-    # def take_damage(self, damage):
-    #     final_damage = max(0, damage - self.defense)
-
-    #     self.current_hp -= final_damage
-
-    #     if self.current_hp < 0:
-    #         self.current_hp = 0
-
-    #     return final_damage
-
-    #хилка
-
-    def heal(self, amount):
-        self.current_hp += amount
-
-        if self.current_hp > self.max_hp:
-            self.current_hp = self.max_hp
-
-  
-    #система маны
-
-    def use_mana(self, amount):
-        if self.current_mana >= amount:
-            self.current_mana -= amount
-            return True
-
-        return False
-
- 
     
     def show_stats(self):
         print("====== PLAYER STATS ======")
