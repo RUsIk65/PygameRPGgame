b26d2295e11d802dc340e638b5ff85b2501ec877
Abilmansur <mansur.orynbasar0506@gmail.com>
2026-05-21 11:52:53 +0500

    Add files via upload

--- RPG/ui/menu.start.py
+++ RPG/ui/menu.start.py
@@ -1,56 +1,71 @@
 import pygame
-from ui.menu import Menu
+from menu import Menu
+from ..main import main1
+
 
 pygame.init()
 
+# =========================
+# SCREEN
+# =========================
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("RPG MENU")
+
 font = pygame.font.SysFont(None, 50)
+
 clock = pygame.time.Clock()
 
-
+# =========================
 # MENU
-
+# =========================
 menu = Menu()
+
 running = True
 
 while running:
 
+    screen.fill('#71ddee')
+
+    # =========================
+    # EVENTS
+    # =========================
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
             running = False
 
-        # КЛАВИАТУРА
         if event.type == pygame.KEYDOWN:
+
+            # UP
             if event.key == pygame.K_UP:
                 menu.move_up()
+
+            # DOWN
             if event.key == pygame.K_DOWN:
                 menu.move_down()
+
+            # ENTER (SELECT)
             if event.key == pygame.K_RETURN:
+
                 action = menu.select()
-                if action == "Start":
-                    # TODO: запустить игру
-                    pass
-                elif action == "Exit":
+
+                print("Selected:", action)
+                match action:
+                    case "start":
+                        main1()
+                    case "setting":
+                        running = False
+                    case "67":
+                        pass
+                # если выйти
+                if action == "Exit":
                     running = False
 
-        # МЫШЬ
-        if event.type == pygame.MOUSEMOTION:
-            menu.handle_mouse_motion(event.pos)
-
-
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            action = menu.handle_mouse_click(event.pos)
-            if action == "Start":
-                # TODO: запустить игру
-                pass
-            elif action == "Exit":
-                running = False
-
-    # DRAW
-
+    # =========================
+    # DRAW MENU
+    # =========================
     menu.draw(screen, font)
+
     pygame.display.update()
     clock.tick(60)
 
